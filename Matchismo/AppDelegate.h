d327928c8bd18fbb0b551cf55a6e7e71eb310fac
ricardo <ricardo.batista07@gmail.com>
2014-12-27 17:56:53 -0400

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Ricardo Batista on 12/27/14.
+//  Copyright (c) 2014 XdroneStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
